6fea08e71683f760d40e5a8c72be668e8d326b5e
Maha Alyemeni <52765342+MahaSaud99@users.noreply.github.com>
2022-09-11 09:40:53 +0300

    Add files via upload

---
+++ src/pages/Result.js
@@ -0,0 +1,40 @@
+import {useNavigate , useParams } from "react-router-dom";
+import Confetti from 'react-confetti'
+import './css/Result.css';
+
+
+const Result=()=>{
+    const param=useParams();
+    const score=param.score;
+    const navigate = useNavigate();
+
+
+
+    const startAgain=()=>{
+      navigate('/')
+    }
+
+ return(
+<div className="div2">
+{ score > 4? 
+( 
+<>
+<Confetti/>
+<p className="title">Congrats!!!</p><br/>
+<p className="score">Your score is <span style={{color:"green"}}>{param.score*10}%</span></p>
+<button type="button" className="btn btn-warning" onClick={startAgain}>Start Again</button>
+</>
+) 
+:
+ (
+    <>
+<p className="score">Your score is <span style={{color:"red"}}>{param.score*10}%</span></p>
+<button type="button" className="btn btn-warning" onClick={startAgain}>Start Again</button>
+
+    </>
+)
+}
+</div>
+ );
+}
+export default Result;
